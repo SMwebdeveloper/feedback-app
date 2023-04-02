58f68aa0e0ea3eee38acd0197ce409ce5a676436
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-04-02 13:12:12 +0500

    sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
 
   <div>
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <HomeView/>
   </div>
 </template>
 <script>
 import HomeView from './views/HomeView.vue';
 export default{
-  comments:{
+  components:{
     HomeView
   }
 }
