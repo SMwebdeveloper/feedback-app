20891b3c72281c2d07ce42afd748770057de194d
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-04-03 17:35:41 +0500

    vue router

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
 
   <div>
-    <HomeView/>
+   
+    <router-view></router-view>
   </div>
 </template>
 <script>
-import HomeView from './views/HomeView.vue';
+// import HomeView from './views/HomeView.vue';
 export default{
-  components:{
-    HomeView
-  }
+  // components:{
+  //   HomeView
+  // }
 }
 </script>
 <style lang="scss">
