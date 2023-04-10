4cdb3edba6716d134b7a4856ed3ac63b67c36a7f
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-04-10 22:39:11 +0500

    loader

---
+++ src/components/UI/Controls/Loader.vue
@@ -0,0 +1,11 @@
+<template lang="">
+  <div class="loader">
+    <svg>
+      <circle cx="70" cy="70" r="70"></circle>
+    </svg>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
