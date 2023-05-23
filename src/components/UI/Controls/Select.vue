b1ee0f4e89945cd23b855929a64520a33b962452
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-05-23 18:07:33 +0500

    select edit

--- src/components/UI/Controls/Select.vue
+++ src/components/UI/Controls/Select.vue
@@ -1,38 +1,9 @@
 <template lang="">
   <div class="control">
     <slot name="label"></slot>
-
-    <div class="control__item control__select">
-      <button
-        type="button"
-        class="control__select--btn"
-        @click="areOptionsVisible = !areOptionsVisible"
-      >
-        <span>{{ selected }}</span>
-
-        <img
-          class="control__btn--icon"
-          :class="`${areOptionsVisible && 'active'}`"
-          src="@/assets/img/arrow-down.png"
-          alt="arrow down"
-        />
-      </button>
-
-      <div class="control__select--list" v-if="areOptionsVisible">
-        <button
-          type="button"
-          class="control__list--item"
-          v-for="item in options"
-          :key="item.id"
-          @click="selecOption(item)"
-        >
-          {{ item.name }}
-          <div v-if="item.type">
-            <img src="@/assets/img/check.svg" alt="check icon" />
-          </div>
-        </button>
-      </div>
-    </div>
+    <select name="" id="" class="control__item control__select">
+      <option v-for="item in options"  :key="item.name" :value="item.name"  >{{item.name}}</option>
+    </select>
   </div>
 </template>
 <script>
@@ -65,11 +36,9 @@ export default {
       this.$emit("input", this.selected);
     },
   },
-  mounted(){
-    if(this.value = ''){
-    }
-    }
-  
+  mounted () {
+    console.log(this.value)
+  }
 };
 </script>
 <style lang=""></style>
