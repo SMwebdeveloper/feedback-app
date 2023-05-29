4aa72a6a4e1056b7689aed61b43344ddf74437a4
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-05-29 17:55:46 +0500

    clean code editFeedback page

--- src/views/editFeedback/index.vue
+++ src/views/editFeedback/index.vue
@@ -10,52 +10,62 @@
         </span>
         Go back
       </router-link>
-      <div  class="feedback-form">
-    <span class="feedback-form__icon feedback-form__icon--second">
-      <img src="@/assets/img/combined.png" alt="combined image" />
-    </span>
-    <h2 class="subtitle">Editing ‘Add a dark theme option’</h2>
-    <form @submit.prevent="editFeedback" v-if="feedback">
-      <AppInput v-model="feedback.title" />
+      <div class="feedback-form">
+        <span class="feedback-form__icon feedback-form__icon--second">
+          <img src="@/assets/img/combined.png" alt="combined image" />
+        </span>
+        <h2 class="subtitle">Editing ‘Add a dark theme option’</h2>
+        <form @submit.prevent="editFeedback" v-if="feedback">
+          <AppInput v-model="feedback.title" />
 
-      <AppSelect :options="typeList" :active="feedback.type" @selected="$event => feedback.type = $event">
-        <template #label>
-          <div>
-            <h2 class="control__title">Category</h2>
-            <p class="descr">Choose a category for your feedback</p>
+          <AppSelect
+            :options="typeList"
+            :active="feedback.type"
+            @selected="($event) => (feedback.type = $event)"
+          >
+            <template #label>
+              <div>
+                <h2 class="control__title">Category</h2>
+                <p class="descr">Choose a category for your feedback</p>
+              </div>
+            </template>
+          </AppSelect>
+
+          <AppSelect
+            :options="statusList"
+            :active="feedback.status"
+            @selected="($event) => (feedback.status = $event)"
+          >
+            <template #label>
+              <div>
+                <h2 class="control__title">Update Status</h2>
+                <p class="descr">Change feature state</p>
+              </div>
+            </template>
+          </AppSelect>
+
+          <AppTextarea v-model="feedback.descr" />
+
+          <div class="feedback-form__btn">
+            <button
+              type="button"
+              @click="deleteFeedback($route.params.id)"
+              class="btn btn__danger"
+            >
+              Delete
+            </button>
+            <router-link
+              :to="{ name: 'feedback', params: { id: feedback.feedbackId } }"
+              class="btn btn__dark"
+              >Cancel</router-link
+            >
+            <button type="submit" class="btn btn__primary">
+              Edit Feedback
+            </button>
           </div>
-        </template>
-      </AppSelect>
-
-      <AppSelect :options="statusList" :active="feedback.status" @selected="$event => feedback.status = $event">
-        <template #label>
-          <div>
-            <h2 class="control__title">Update Status</h2>
-            <p class="descr">Change feature state</p>
-          </div>
-        </template>
-      </AppSelect>
-
-      <AppTextarea v-model="feedback.descr" />
-
-      <div class="feedback-form__btn">
-        <button
-          type="button"
-          @click="deleteFeedback($route.params.id)"
-          class="btn btn__danger"
-        >
-          Delete
-        </button>
-        <router-link
-          :to="{ name: 'feedback', params: { id: feedback.feedbackId } }"
-          class="btn btn__dark"
-          >Cancel</router-link
-        >
-        <button type="submit" class="btn btn__primary">Edit Feedback</button>
+        </form>
+        <Loader v-else />
       </div>
-    </form>
-    <Loader v-else/>
-  </div>
     </div>
   </div>
 </template>
@@ -63,7 +73,7 @@
 import AppInput from "@/components/UI/Controls/Input";
 import AppSelect from "@/components/UI/Controls/Select";
 import AppTextarea from "@/components/UI/Controls/Textarea";
-import Loader from "@/components/UI/Controls/Loader"
+import Loader from "@/components/UI/Controls/Loader";
 import { mapActions, mapGetters } from "vuex";
 import axios from "axios";
 export default {
@@ -71,13 +81,13 @@ export default {
     AppInput,
     AppSelect,
     AppTextarea,
-    Loader
+    Loader,
   },
   data() {
     return {
       typeList: ["Feature", "UI", "UX", "Enhancement", "Bug"],
       statusList: ["Suggestion", "Planned", "In-Progress", "Live"],
-      feedback: null
+      feedback: null,
     };
   },
   computed: {
@@ -90,7 +100,7 @@ export default {
       fetchFeedback: "fetchFeedback",
     }),
     editFeedback() {
-      console.log(this.feedback.type)
+      console.log(this.feedback.type);
       this.$store
         .dispatch("editFeedback", this.feedback)
         .then(() => this.$router.push(`/feedback/${this.feedback.feedbackId}`));
@@ -105,9 +115,9 @@ export default {
     },
   },
   async mounted() {
-   await this.fetchFeedback(this.$route.params.id);
-    this.feedback = this.getFeedback
-  }
+    await this.fetchFeedback(this.$route.params.id);
+    this.feedback = this.getFeedback;
+  },
 };
 </script>
 <style lang=""></style>
