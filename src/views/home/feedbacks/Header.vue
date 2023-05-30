e2a942c3110ad62350dfa8f24805139885d85516
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-05-30 10:01:38 +0500

    fix single feedback home

---
+++ src/views/home/feedbacks/Header.vue
@@ -0,0 +1,57 @@
+<template lang="">
+  <div class="feedback-header">
+    <div class="feedback-header__content">
+      <div class="feedback-header__content--suggetions">
+        <img src="@/assets/img/suggetion.svg" alt="suggetion image" />
+        <h4 class="feedback-header__content--suggetions__title">
+          <span>{{ feedbacksLength }}</span>
+          Suggetions
+        </h4>
+      </div>
+
+      <Select :options="selectOptions" @selectOption="selectOption" />
+    </div>
+
+    <router-link to="/create" class="btn btn__primary"
+      >+Add Feedback</router-link
+    >
+  </div>
+</template>
+<script>
+import Select from "@/components/feedback/Select";
+import { mapGetters } from "vuex";
+export default {
+  name: "Feedback-header",
+  components: {
+    Select,
+  },
+  props: {
+    fedbacksLength: {
+      typeof: Number,
+      default: 0,
+    },
+  },
+  data() {
+    return {
+      selectOptions: [
+        { name: "Most Upvotes", value: "mostUpvotes", check: true },
+        { name: "Least Upvotes", value: "leastUpvotes", check: false },
+        { name: "Most Comments", value: "mostComments", check: false },
+        { name: "Least Comments", value: "leastComments", check: false },
+      ],
+    };
+  },
+  methods: {
+    selectOption(item) {
+      this.selected = item.name;
+      this.areOptionsVisible = false;
+    },
+  },
+  computed: {
+    ...mapGetters({
+      feedbacksLength: "getFeedbacksLength",
+    }),
+  },
+};
+</script>
+<style lang=""></style>
