a51e56f0d1ae93a3a16457b72ad01f201e5a62e2
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-05-30 10:27:35 +0500

    fix single ui controls

---
+++ src/components/Controls/Input.vue
@@ -0,0 +1,32 @@
+<template lang="">
+  <div class="control">
+    <h5 class="control__title">Feedback Title</h5>
+    <p class="descr">Add a short, descriptive headline</p>
+    <input
+      class="control__item"
+      v-bind="$attrs"
+      :type="type"
+      :value="value"
+      @input="$emit('input', $event.target.value)"
+    />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: "",
+    },
+    type: {
+      type: String,
+      default: "text",
+    },
+    required: {
+      type: Boolean,
+      default: true,
+    },
+  },
+};
+</script>
+<style lang=""></style>
---
+++ src/components/Controls/Loader.vue
@@ -0,0 +1,9 @@
+<template lang="">
+  <div class="loader">
+    <div class="loader__item"></div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
