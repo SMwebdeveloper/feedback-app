eb0eba6fcf216f218df5b9b56bf2d4084d71e5a2
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-05-30 10:02:01 +0500

    fix single sidebar home

---
+++ src/views/home/sidebar/Navigation.vue
@@ -0,0 +1,41 @@
+<template lang="">
+  <div class="sidebar__item sidebar__item--filter">
+    <button
+      class="sidebar__btn"
+      v-for="tab in tabs"
+      :key="tab"
+      :class="{'active': tab === active }"
+      @click="active = tab"
+    >
+      {{ tab }}
+    </button>
+  </div>
+</template>
+<script>
+import { mapActions } from "vuex";
+
+export default {
+  data() {
+    return {
+      active: 'All',
+      tabs: ['All', 'UI', 'UX', 'Enhancement', 'Bug', 'Feature' ]
+    };
+  },
+  watch: {
+    active: {
+      handler (value) {
+        this.filterFeedbacks(value)
+      },
+      immediate: true
+    }
+  },
+  methods: {
+    ...mapActions({
+      setSidebarTab: "setSidebarTab",
+      filterFeedbacks: 'filterFeedbacks'
+    })
+  }
+};
+</script>
+<style lang="">
+</style>
