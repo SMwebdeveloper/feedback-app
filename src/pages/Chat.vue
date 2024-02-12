931e898f94bac94d08fd62bfc6a761131c4124c8
Samandar <samandarwebdeveloper@gmail.com>
2024-02-12 23:00:19 +0500

    add chat s

---
+++ src/pages/Chat.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="project-container">
+    <a>
+      <chevron-left-icon
+        @click="router.back()"
+        class="text-white w-10 font-medium mb-5 cursor-pointer"
+      />
+    </a>
+    <form>
+        <input v-model="message" type="text">
+        <button click="addMessage">add</button>
+    </form>
+  </div>
+</template>
+<script setup lang="ts">
+import {useRouter} from 'vue-router'
+import {ref} from 'vue'
+// import { database } from '@/firebase/config';
+import { set, ref as dataRef, set, get, child, onValue, getDatabase } from 'firebase/database'
+const router = useRouter()
+const message = ref('')
+const db = dataRef(getDatabase())
+
+const addMessage = () => {
+    
+}
+</script>
--- src/pages/Chats.vue
+++ src/pages/Chats.vue
@@ -1,13 +1,45 @@
 <template>
-    <section>
-        <div class="project-container relative">
-            <h2 class="text-white text-3xl font-bold absolute top-1/2 left-1/3 z-10 drop-shadow-lg">Soon</h2>
-            <ul class="blur-sm">
-                <li v-for="item in 7" :key="item" class="flex items-center border-b-2 border-whtie py-4">
-                    <img src="@/assets/images/user-image.jpg" alt="user image" class="w-10 h-10 rounded-full mr-8">
-                    <h2 class="text-white text-2xl font-bold">Samandar</h2>
-                </li>
-            </ul>
-        </div>
-    </section>
+  <section>
+    <div class="project-container relative">
+      <h2
+        class="text-white text-3xl font-bold absolute top-1/2 left-1/3 z-10 drop-shadow-lg"
+      >
+        Soon
+      </h2>
+      <ul class="blur-sm">
+        <li
+          v-for="item in 7"
+          :key="item"
+          class="flex items-center border-b-2 border-whtie py-4"
+        >
+          <img
+            src="@/assets/images/user-image.jpg"
+            alt="user image"
+            class="w-10 h-10 rounded-full mr-8"
+          />
+          <h2 class="text-white text-2xl font-bold">Samandar</h2>
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
+<script setup lang="ts">
+// import {database} from '@/firebase/config'
+import { ref as dataRef, get, child, getDatabase } from "firebase/database";
+import { onMounted, ref } from "vue";
+
+const db = dataRef(getDatabase());
+const allChats = ref();
+onMounted(async () => {
+  await get(child(db, "allChats")).then((snapshot: any) => {
+    if (!snapshot.exists()) {
+      console.log("Not found chats");
+    }
+    allChats.value = Object.entries(snapshot.val()).map((item: any) => {
+      const [key, value] = item;
+      return { ...value, chatId: key };
+    });
+    localStorage.setItem("allChats", allChats.value);
+  });
+});
+</script>
